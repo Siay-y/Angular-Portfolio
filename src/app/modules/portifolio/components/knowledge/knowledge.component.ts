a22d92cc1988a3a8d1034bc4be9b35fcb92c5c60
Siay <125613204+Siay-y@users.noreply.github.com>
2024-01-23 16:59:00 -0300

    HTML-knowledges-component+updates

---
+++ src/app/modules/portifolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,31 @@
+import { Component, signal } from '@angular/core';
+
+import { IKnowledge } from '../../interface/IKnowledge.interface';
+
+@Component({
+  selector: 'app-knowledge',
+  standalone: true,
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: 'assets/icons/knowledge/html.svg',
+      alt: 'icone de conhecimento de HTML 5'
+    },
+    {
+      src: 'assets/icons/knowledge/css.svg',
+      alt: 'icone de conhecimento de CSS 3'
+    },
+    {
+      src: 'assets/icons/knowledge/js.svg',
+      alt: 'icone de conhecimento de java script'
+    },
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'icone de conhecimento de angular'
+    }
+  ]);
+}
