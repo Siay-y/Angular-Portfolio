f4c0627df8acdd05f6d9aeedfebec931d72f8130
Siay <125613204+Siay-y@users.noreply.github.com>
2024-01-26 12:18:08 -0300

    FULL-CSS+angular-material V.1.0.0

--- src/app/modules/portifolio/components/experiences/experiences.component.ts
+++ src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -13,17 +13,24 @@ export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
       summary:{
-        strong: 'none',
-        p: 'none',
+        strong: 'Experiência I',
+        p: 'none | Jan 2024',
       },
-      text: '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>',
+      text: '<p>Lorem ipsum dolor sit amet. Aut dolorum laboriosam in unde rerum ut fuga libero rem esse tenetur qui sunt neque. Qui dolores ducimus qui quod magni ea voluptas cumque est sint illo vel praesentium reiciendis et voluptatem excepturi. Qui vero libero eos autem officia et corporis blanditiis aut quia molestiae hic quas nisi et suscipit perferendis et sequi nisi.<br><br>Qui corrupti veritatis sit sint expedita cum autem nihil et distinctio temporibus eum voluptas repellendus cum tempore distinctio. At dolorum ullam et corrupti quibusdam quo facere quas qui consequatur explicabo aut sint ipsum? Quo aliquam earum aut perferendis voluptatem qui quam enim aut quasi saepe. Ut quam eius et tempore quam in quasi perspiciatis sed voluptatibus amet eos libero accusantium est quidem atque 33 illo reprehenderit.</p>',
     },
     {
       summary:{
-        strong: 'none',
-        p: 'none',
+        strong: 'Experiência II',
+        p: 'none | Jan 2024',
       },
-      text: '<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>',
+      text: '<p>Lorem ipsum dolor sit amet. Aut dolorum laboriosam in unde rerum ut fuga libero rem esse tenetur qui sunt neque. Qui dolores ducimus qui quod magni ea voluptas cumque est sint illo vel praesentium reiciendis et voluptatem excepturi. Qui vero libero eos autem officia et corporis blanditiis aut quia molestiae hic quas nisi et suscipit perferendis et sequi nisi.<br><br>Qui corrupti veritatis sit sint expedita cum autem nihil et distinctio temporibus eum voluptas repellendus cum tempore distinctio. At dolorum ullam et corrupti quibusdam quo facere quas qui consequatur explicabo aut sint ipsum? Quo aliquam earum aut perferendis voluptatem qui quam enim aut quasi saepe. Ut quam eius et tempore quam in quasi perspiciatis sed voluptatibus amet eos libero accusantium est quidem atque 33 illo reprehenderit.</p>',
+    },
+    {
+      summary:{
+        strong: 'Experiência II',
+        p: 'none | Jan 2024',
+      },
+      text: '<p>Lorem ipsum dolor sit amet. Aut dolorum laboriosam in unde rerum ut fuga libero rem esse tenetur qui sunt neque. Qui dolores ducimus qui quod magni ea voluptas cumque est sint illo vel praesentium reiciendis et voluptatem excepturi. Qui vero libero eos autem officia et corporis blanditiis aut quia molestiae hic quas nisi et suscipit perferendis et sequi nisi.<br><br>Qui corrupti veritatis sit sint expedita cum autem nihil et distinctio temporibus eum voluptas repellendus cum tempore distinctio. At dolorum ullam et corrupti quibusdam quo facere quas qui consequatur explicabo aut sint ipsum? Quo aliquam earum aut perferendis voluptatem qui quam enim aut quasi saepe. Ut quam eius et tempore quam in quasi perspiciatis sed voluptatibus amet eos libero accusantium est quidem atque 33 illo reprehenderit.</p>',
     },
   ]);
 }
--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -1,22 +1,30 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
 import { IProjects } from '../../interface/IProjects.interface';
+import { DialogProjectComponent } from '../dialog/dialog-project/dialog-project.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
   public arrayProjects = signal<IProjects[]>([
     {
-      src: 'assets/img/projects/teste.jpg',
+      src: '../../../../../assets/icons/idk.svg',
       alt: 'apenas um teste, ainda não há nada aqui...',
       title: 'Teste',
-      width: '110px',
-      height: '100px',
+      width: '40px',
+      height: '40px',
       description:
       '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>',
       links: [
@@ -25,6 +33,43 @@ export class ProjectsComponent {
           href: '',
         }
       ]
-    }
-  ])
+    },
+    {
+      src: '../../../../../assets/icons/idk.svg',
+      alt: 'apenas um teste, ainda não há nada aqui...',
+      title: 'Teste',
+      width: '40px',
+      height: '40px',
+      description:
+      '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>',
+      links: [
+        {
+          name: 'Conheça o sitema',
+          href: '',
+        }
+      ]
+    },
+    {
+      src: '../../../../../assets/icons/idk.svg',
+      alt: 'apenas um teste, ainda não há nada aqui...',
+      title: 'Teste',
+      width: '40px',
+      height: '40px',
+      description:
+      '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>',
+      links: [
+        {
+          name: 'Conheça o sitema',
+          href: '',
+        }
+      ]
+    },
+  ]);
+
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectComponent, {
+      data,
+      panelClass: EDialogPanelClass.projects
+    })
+  }
 }
